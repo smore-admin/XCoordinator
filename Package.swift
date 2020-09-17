24792bf1f30dd96376e16234e4461c6c6f94aee0
Paul <pauljohanneskraft@users.noreply.github.com>
2020-09-17 04:52:02 +0200

    Remove Platforms from Package.swift

--- Package.swift
+++ Package.swift
@@ -5,7 +5,6 @@ import PackageDescription
 
 let package = Package(
     name: "XCoordinator",
-    platforms: [.iOS(.v8)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
